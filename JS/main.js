f00ee3e66a187407a21f1babc32bb1a3436444d7
noemiyuen <noemiyuen@gmail.com>
2022-10-19 10:55:54 +0200

    Add Template string (interpolar)

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,7 @@ formElement.classList.remove("collapsed");
 
 const listElement = document.querySelector(".js-list");
 
-const kittenOne = `<li class="card">
+/* const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
@@ -50,6 +50,71 @@ const kittenThree = `<li class="card">
 </p>
 </li>`;
 
-const content = kittenOne + kittenTwo + kittenThree;
+const content = kittenOne + kittenTwo + kittenThree; 
+ 
+listElement.innerHTML = content;  */
 
-listElement.innerHTML = content;
+const kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+const kittenImage2 = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const kittenImage3 = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+
+const kittenName1 = 'Anastacio';
+const kittenName2 = 'Fiona';
+const kittenName3 = 'Cielo';
+
+const kittenDesc1 =  'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const kittenDesc2 = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+
+const kittenDesc3 = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+
+const kittenRace1 ='British Shorthair';
+const kittenRace2 ='British Shorthair';
+const kittenRace3 ='British Shorthair';
+
+
+
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="${kittenImage1}"
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRace1}</h4>
+  <p class="card_description">
+  ${kittenDesc1}
+  </p>
+</article>
+</li>`;
+
+const kittenTwo = ` <li class="card">
+<img
+  class="card_img"
+  src="${kittenImage2}"
+  alt="gatito"
+/>
+<h3 class="card_title">${kittenName2.toUpperCase()}</h3>
+<h4 class="card_race">${kittenRace2}</h4>
+<p class="card_description">
+${kittenDesc2}
+</p>
+</li>`;
+
+const kittenThree = `<li class="card">
+<img
+  class="card_img"
+  src="${kittenImage3}"
+  alt="gatito"
+/>
+<h3 class="card_title">${kittenName3.toUpperCase()}</h3>
+<h4 class="card_race">${kittenRace3}</h4>
+<p class="card_description">
+${kittenDesc3}
+</p>
+</li>`;
+
+const content = kittenOne + kittenTwo + kittenThree; 
+ 
+listElement.innerHTML = content; 
