cf1faab54d01c0f95a287ee978563b8ab088b8b3
Ana Lozano Moreno <analozanomor@gmail.com>
2022-10-28 11:27:46 +0200

    update bucles

--- JS/main.js
+++ JS/main.js
@@ -1,58 +1,8 @@
 'use strict';
 
-/* const formElement = document.querySelector('.js-new-form'); */
-
-// formElement.classList.remove('collapsed');
+//llamar elementos HTML y declarar constantes
 
 const listElement = document.querySelector('.js-list');
-
-/* const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
-    alt="gatito"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">British Shorthair</h4>
-  <p class="card_description">
-    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
-    moleste. Es una maravilla acariciarle!
-  </p>
-</article>
-</li>`;
-
-const kittenTwo = ` <li class="card">
-<img
-  class="card_img"
-  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
-  alt="gatito"
-/>
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
-</p>
-</li>`;
-
-const kittenThree = `<li class="card">
-<img
-  class="card_img"
-  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
-  alt="gatito"
-/>
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
-</p>
-</li>`;
-
-const content = kittenOne + kittenTwo + kittenThree; 
- 
-listElement.innerHTML = content;  */
 
 const kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 const kittenImage2 =
@@ -76,108 +26,6 @@ const kittenRace1 = 'British Shorthair';
 const kittenRace2 = 'British Shorthair';
 const kittenRace3 = 'British Shorthair';
 
-/*
-const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="${kittenImage1}"
-    alt="gatito"
-  />
-  <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
-  <h4 class="card_race">${kittenRace1}</h4>
-  <p class="card_description">
-  ${kittenDesc1}
-  </p>
-</article>
-</li>`;
-
-const kittenTwo = ` <li class="card">
-<img
-  class="card_img"
-  src="${kittenImage2}"
-  alt="gatito"
-/>
-<h3 class="card_title">${kittenName2.toUpperCase()}</h3>
-<h4 class="card_race">${kittenRace2}</h4>
-<p class="card_description">
-${kittenDesc2}
-</p>
-</li>`;
-
-const kittenThree = `<li class="card">
-<img
-  class="card_img"
-  src="${kittenImage3}"
-  alt="gatito"
-/>
-<h3 class="card_title">${kittenName3.toUpperCase()}</h3>
-<h4 class="card_race">${kittenRace3}</h4>
-<p class="card_description">
-${kittenDesc3}
-</p>
-</li>`;
-*/
-// const content = kittenOne + kittenTwo + kittenThree;
-
-// listElement.innerHTML = content;
-
-/*
-const input_search_desc = document.querySelector('.js_in_search_desc');
-
-input_search_desc.value = '';
-
-const descrSearchText = input_search_desc.value;
-
-if (kittenDesc1.includes(descrSearchText)) {
-  listElement.innerHTML += kittenOne;
-}
-
-if (kittenDesc2.includes(descrSearchText)) {
-  listElement.innerHTML += kittenTwo;
-}
-
-if (kittenDesc3.includes(descrSearchText)) {
-  listElement.innerHTML += kittenThree;
-}
-
-*/
-
-// formElement.classList.add('collapsed');
-/* CORTAMOS ESTO PARA EL EJERCICIO EVENTOS
-
-if (formElement.classList.contains('collapsed')) {
-  formElement.classList.remove('collapsed');
-} else {
-  formElement.classList.add('collapsed');
-}
-*/
-
-// BONUS NO FINALIZADO COMPROBAR
-/*
-let html = '';
-const inputRaza = document.querySelector('.js-input');
-inputRaza.value = html;
-if (kittenRace1 === '') {
-  html = `No se ha especificado la raza`;
-  inputRaza.value = html;
-} else {
-  html = race;
-}
-*/
-
-// const plusElement = document.querySelector('.js-plus');
-
-// plusElement.addEventListener('click', (event) => {
-//   event.preventDefault();
-
-//   if (formElement.classList.contains('collapsed')) {
-//     formElement.classList.remove('collapsed');
-//   } else {
-//     formElement.classList.add('collapsed');
-//   }
-// });
-
 const añadirElement = document.querySelector('.js-btn-add');
 const inputDesc = document.querySelector('.js-input-desc');
 const inputPhoto = document.querySelector('.js-input-photo');
@@ -194,40 +42,6 @@ añadirElement.addEventListener('click', (event) => {
     validationElement.innerHTML = 'Debe rellenar todos los valores';
   }
 });
-
-/* 
-COMENTADO PARA EL EJERICCIO FUNCIONES II
-
-const searchElement = document.querySelector('.js-search');
-const inputRaceElement = document.querySelector('.js-inputRace');
-const searchValidatorElement = document.querySelector('.js-searchValidator');
-
-searchElement.addEventListener('click', (event) => {
-  const searchDescValue = input_search_desc.value;
-  const searchRaceValue = inputRaceElement.value;
-  if (searchDescValue === '' || searchRaceValue === '') {
-    searchValidatorElement.innerHTML = 'Debe rellenar todos los valores';
-  }
-});
- */
-// const cancelElement = document.querySelector('.js-cancel');
-
-// cancelElement.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   formElement.classList.add('collapsed');
-//   const valueDesc = inputDesc.value;
-//   const valuePhoto = inputPhoto.value;
-//   const valueName = inputName.value;
-//   valueDesc = '';
-//   valuePhoto = '';
-//   valueName = '';
-// });
-//SOPORTE:
-/* 
-   ESTA BIEN PONER VALOR ="" O EXISTE FC RESET
-   PARA QUITAR REQUIRED DE CADA INPUT PARA QUE NOS DEJE OCULTAR
-   BUSCAR: NO SE QUEDA EL PARRAFO DE LA VALIDACIÓN SE BORRA RÁPIDO.
-   */
 
 const newFormElement = document.querySelector('.js-new-form');
 
@@ -249,74 +63,7 @@ function handleClickNewCatForm(event) {
 }
 linkNewFormElememt.addEventListener('click', handleClickNewCatForm);
 
-// function renderKitten(url, desc, nam, race) {
-//   const includeArticle = (listElement.innerHTML += `<li class="card">
-//   <article>
-//     <img
-//       class="card_img"
-//       src="${url}"
-//       alt="gatito"
-//     />
-//     <h3 class="card_title">${nam.toUpperCase()}</h3>
-//     <h4 class="card_race">${race}</h4>
-//     <p class="card_description">
-//     ${desc}
-//     </p>
-//   </article>
-//   </li>`);
-//   return includeArticle;
-// }
-
-/* 
-renderKitten(kittenImage1, kittenDesc1, kittenName1, kittenRace1);
-renderKitten(kittenImage2, kittenDesc2, kittenName2, kittenRace2);
-renderKitten(kittenImage3, kittenDesc3, kittenName3, kittenRace3);
- */
-
-//EJERECICIO FUNCIONES 1
-
-/*
-const input_search_desc = document.querySelector('.js_in_search_desc');
-
-input_search_desc.value = '';
-
-const descrSearchText = input_search_desc.value;
-
-if (kittenDesc1.includes(descrSearchText)) {
-  listElement.innerHTML = renderKitten(
-    kittenImage1,
-    kittenDesc1,
-    kittenName1,
-    kittenRace1
-  );
-}
-
-if (kittenDesc2.includes(descrSearchText)) {
-  listElement.innerHTML = renderKitten(
-    kittenImage2,
-    kittenDesc2,
-    kittenName2,
-    kittenRace2
-  );
-}
-
-if (kittenDesc3.includes(descrSearchText)) {
-  listElement.innerHTML = renderKitten(
-    kittenImage3,
-    kittenDesc3,
-    kittenName3,
-    kittenRace3
-  );
-}*/
 const buttonAdd = document.querySelector('.js-btn-add');
-
-//modifica el evento para cumplir una función manejadora
-
-/* CODIGO YA CREADO
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-*/
 
 const inputRace = document.querySelector('.js-input-race');
 
@@ -395,9 +142,7 @@ const input_search_desc = document.querySelector('.js_in_search_desc');
 const descrSearchText = input_search_desc.value;
 const buttonSearch = document.querySelector('.js-button-search');
 
-/* if(input_search_desc.value===''){
-  listElement.innerHTML = content;
-} */ //PINTA LOS GATOS DOBLE
+//PINTA LOS GATOS DOBLE
 
 const filterKitten = (event) => {
   event.preventDefault();
@@ -413,25 +158,8 @@ const filterKitten = (event) => {
   }
 };
 
-// const searchBox = document.querySelector('.js-search');
-
-// const inputRaceElement = document.querySelector('.js-inputRace');
-// const renderRace = (text) => {
-//   const raceValue = inputRaceElement.value;
-//   if (raceValue === '') {
-//     searchBox.innerHTML += `<p class="card_race">No se ha especificado la raza</p>`;
-//   } else {
-//     searchBox.innerHTML += `<h3 class="card_race">race</h3>`;
-//   }
-// };
-
 buttonSearch.addEventListener('click', filterKitten);
-buttonSearch.addEventListener('click', renderRace);
-
-/*
-<h3 class="card_race">race</h3>
-<p class="card_race">No se ha especificado la raza</p>
-*/
+// buttonSearch.addEventListener('click', renderRace); NO Incluida en este fichero js ver en copy
 
 //Creando objetos
 const kittenData_1 = {
@@ -455,35 +183,84 @@ const kittenData_3 = {
 
 //function renderKitten Comentada en la linea 252
 
-function renderKitten(kittenData) {
-  const includeArticle = (listElement.innerHTML += `<li class="card">
-      <article>
-        <img
-         class="card_img"
-          src="${kittenData.kittenImage}"
-           alt="gatito"
-        />
-       <h3 class="card_title">${kittenData.KittenName.toUpperCase()}</h3>
-        <h4 class="card_race">${kittenData.KittenRace}</h4>
-      <p class="card_description">
-      ${kittenData.kittenDesc}
-      </p>
-     </article>
-       </li>`);
-  return includeArticle;
-}
+// function renderKitten(kittenData) {
+//   const includeArticle = (listElement.innerHTML += `<li class="card">
+//       <article>
+//         <img
+//          class="card_img"
+//           src="${kittenData.kittenImage}"
+//            alt="gatito"
+//         />
+//        <h3 class="card_title">${kittenData.KittenName.toUpperCase()}</h3>
+//         <h4 class="card_race">${kittenData.KittenRace}</h4>
+//       <p class="card_description">
+//       ${kittenData.kittenDesc}
+//       </p>
+//      </article>
+//        </li>`);
+//   return includeArticle;
+// }
 
 //TEMA ARRAYS
 
-const kittenDataList = [kittenData_1,kittenData_2,kittenData_3]
+const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
 
 //TEMA BUCLES
 
+//Completa el código:
+//Vaciamos el innerHTML del <ul> de la página.
+//Iteramos sobre el listado de gatitos
+//Y por cada iteración pintamos un gatito.
+//utilizando la función renderKitten(kittenItem)
+let kittenIndex = '0';
 function renderKittenList(kittenDataList) {
-  
-  //Completa el código:
-  //Vaciamos el innerHTML del <ul> de la página.
-  //Iteramos sobre el listado de gatitos
-  //Y por cada iteración pintamos un gatito.
-  //utilizando la función renderKitten(kittenItem)
+  listElement.innerHTML = '';
+  for (let index = 0; index < kittenDataList.length; index++) {
+    kittenIndex += kittenDataList[index];
+  }
+  function renderKitten(kittenIndex) {
+    const includeArticle = (listElement.innerHTML += `
+      <li class="card">
+        <article>
+          <img
+          class="card_img"
+          src="${kittenIndex.image}"
+          alt="gatito"
+          />
+          <h3 class="card_title">${kittenIndex.name.toUpperCase()}</h3>
+          <h4 class="card_race">${kittenIndex.race}</h4>
+          <p class="card_description">
+          ${kittenIndex.desc}
+          </p>
+        </article>
+       </li>`);
+    return includeArticle;
+  }
 }
+renderKittenList(kittenDataList);
+
+/* 
+  for (let index = 0; index < kittenDataList.length; index++) {
+    function renderKitten(kittenData) {
+      console.log(kittenDataList[index].image);
+
+      const includeArticle = (listElement.innerHTML += `<li class="card">
+        <article>
+          <img
+           class="card_img"
+            src="${kittenDataList[index].image}"
+             alt="gatito"
+          />
+         <h3 class="card_title">${kittenData.KittenName.toUpperCase()}</h3>
+          <h4 class="card_race">${kittenData.KittenRace}</h4>
+        <p class="card_description">
+        ${kittenData.kittenDesc}
+        </p>
+       </article>
+         </li>`);
+      return includeArticle;
+    }
+  }
+}
+renderKittenList(kittenDataList);
+ */
